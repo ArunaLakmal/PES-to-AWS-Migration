0c69e7aa394f59c5855b94d696d22a9cef28fb3b
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-19 17:08:16 +0530

    added RDS Subnets

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 provider "aws" {
-    region = "${var.aws_region}"
+  region = "${var.aws_region}"
 }
+
 #---- VPC ----
 resource "aws_vpc" "pes_vpc" {
-  cidr_block = "${var.vpc_cidr}"
+  cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
-      Name = "pes_vpc"
+    Name = "pes_vpc"
   }
 }
 
@@ -17,22 +18,22 @@ resource "aws_internet_gateway" "pes_igw" {
   vpc_id = "${aws_vpc.pes_vpc.id}"
 
   tags = {
-      Name = "pes_igw"
+    Name = "pes_igw"
   }
 }
 
 resource "aws_eip" "pes_eip" {
-  vpc = true 
+  vpc        = true
   depends_on = ["aws_internet_gateway.pes_igw"]
 }
 
 resource "aws_nat_gateway" "pes_nat_gw" {
   allocation_id = "${aws_eip.pes_eip.id}"
-  subnet_id = "${aws_subnet.pes_public2_subnet.id}"
-  depends_on = ["aws_internet_gateway.pes_igw"]
+  subnet_id     = "${aws_subnet.pes_public2_subnet.id}"
+  depends_on    = ["aws_internet_gateway.pes_igw"]
 
   tags = {
-      Name = "pes_nat_gw"
+    Name = "pes_nat_gw"
   }
 }
 
@@ -41,12 +42,12 @@ resource "aws_route_table" "pes_public_rt" {
   vpc_id = "${aws_vpc.pes_vpc.id}"
 
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.pes_igw.id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.pes_igw.id}"
   }
 
   tags = {
-      Name = "pes_public"
+    Name = "pes_public"
   }
 }
 
@@ -55,46 +56,56 @@ resource "aws_route_table" "pes_public_rt" {
 #---- Subnets ----
 
 resource "aws_subnet" "pes_public1_subnet" {
-  vpc_id = "${aws_vpc.pes_vpc.id}"
-  cidr_block = "${var.cidrs["public1"]}"
+  vpc_id                  = "${aws_vpc.pes_vpc.id}"
+  cidr_block              = "${var.cidrs["public1"]}"
   map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
 
   tags = {
-      Name = "pes_public1"
+    Name = "pes_public1"
   }
 }
 
 resource "aws_subnet" "pes_public2_subnet" {
-  vpc_id = "${aws_vpc.pes_vpc.id}"
-  cidr_block = "${var.cidrs["public2"]}"
+  vpc_id                  = "${aws_vpc.pes_vpc.id}"
+  cidr_block              = "${var.cidrs["public2"]}"
   map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
 
   tags = {
-      Name = "pes_public2"
+    Name = "pes_public2"
   }
 }
 
 resource "aws_subnet" "pes_private1_subnet" {
-  vpc_id = "${aws_vpc.pes_vpc.id}"
-  cidr_block = "${var.cidrs["private1"]}"
+  vpc_id                  = "${aws_vpc.pes_vpc.id}"
+  cidr_block              = "${var.cidrs["private1"]}"
   map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
 
   tags = {
-      Name = "pes_private1"
+    Name = "pes_private1"
   }
 }
 
 resource "aws_subnet" "pes_private2_subnet" {
-  vpc_id = "${aws_vpc.pes_vpc.id}"
-  cidr_block = "${var.cidrs["private2"]}"
+  vpc_id                  = "${aws_vpc.pes_vpc.id}"
+  cidr_block              = "${var.cidrs["private2"]}"
   map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
 
   tags = {
-      Name = "pes_private2"
+    Name = "pes_private2"
   }
 }
 
+resource "aws_subnet" "pes_rds_subnet" {
+  vpc_id                  = "${aws_vpc.pes_vpc.id}"
+  cidr_block              = "${var.cidrs["rds1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags = {
+    Name = "pes_rds"
+  }
+}
